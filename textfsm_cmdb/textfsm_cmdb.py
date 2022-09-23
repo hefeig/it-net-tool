debf5810c0e542e84e43a8012fcaf180dda04543
hefeig <hefeii@foxmail.com>
2022-09-23 18:38:17 +0800

    9/23

--- textfsm_cmdb/textfsm_cmdb.py
+++ textfsm_cmdb/textfsm_cmdb.py
@@ -3,34 +3,37 @@
 # @Time : 2022/8/25 11:37 
 # @Author : HEFEI
 # @File : 3jia.py
-
 import os
 from textfsm import parser, TextFSM
 import time
 import threading
 import queue
 import getpass
-from netmiko import ConnectHandler, NetmikoTimeoutException, NetMikoAuthenticationException, ConfigInvalidException, NetMikoTimeoutException
+from netmiko import ConnectHandler, NetmikoTimeoutException, NetMikoAuthenticationException, ConfigInvalidException, \
+    NetMikoTimeoutException
 from openpyxl import Workbook, load_workbook
 import pandas as pd
 # from netmiko import exceptions
-#
+import logging
 
 
 # 创建CMDB表格,初始化；
-def INIT_CMDB():
-    global log1
+def INIT_CMDB(log1):
     # 创建配置收集文件夹
-    log1 = 'logs/' + time.strftime('日志_%Y年%m月%d日%H点%M分', time.localtime())
-    if not os.path.exists(log1):
-        os.makedirs(log1)
-        print(f'{log1} 文件夹创建完成')
+    # log2 = 'logs/' + time.strftime('日志_%Y年%m月%d日%H点%M分', time.localtime())
+    # if not os.path.exists(log1):
+    #     os.makedirs(log1)
+    #     print(f'{log1} 文件夹创建完成')
+    #     logger.debug(f'{log1} 文件夹创建完成')
     if os.path.exists(f'{log1}/不可达设备.txt'):
         os.remove(f'{log1}/不可达设备.txt')
+        logger.debug(f'{log1}/不可达设备.txt创建完成')
     if os.path.exists(f'{log1}/认证失败设备.txt'):
         os.remove(f'{log1}/认证失败设备.txt')
+        logger.debug(f'{log1}/认证失败设备.txt创建完成')
     if os.path.exists('CMDB.xlsx'):
         os.remove('CMDB.xlsx')
+        logger.debug(f'已删除旧的CMDB.xlsx文件')
     workbook = Workbook('CMDB.xlsx')
     sheet = workbook.create_sheet('主机（NET）')
     # cells=sheet[sheet.dimensions]
@@ -50,6 +53,7 @@ def INIT_CMDB():
         '上联接口'
     ])
     workbook.save('CMDB.xlsx')
+    logger.debug('创建了一个新的CMDB.xlsx文件')
 
 
 def h3c_tm(ip, text_output, dev):
@@ -117,21 +121,26 @@ def h3c_tm(ip, text_output, dev):
                   UP_PORT
                   ])
     workbook.save('CMDB.xlsx')
+    logger.debug(f"{ip} {data['HOSTNAME']}录入完成")
 
 
 class ssh_method(threading.Thread):
-    def __init__(self, devicety, ip, username, passwd, queue):
+    def __init__(self, devicety, ip, username, passwd, enablePW, queue):
         threading.Thread.__init__(self)
         # self.Opr = Opr
         self.devicety = devicety
         self.ip = ip
         self.username = username
-        self.passwd =passwd
+        self.passwd = passwd
         self.queue = queue
+        self.enablePasswd = enablePW
+
     def run(self):
         try:
             # 生成本地时间
             # date_time = datetime.datetime.now().strftime("%Y-%m-%d")
+            # 需要enable密码的厂商
+            enableMa = ['ruijie_os', 'cisco_ios']
             h3c_cmdlist = [
                 '\n',
                 'disp lldp nei list',
@@ -153,8 +162,22 @@ class ssh_method(threading.Thread):
                 cmdlist = h3c_cmdlist
             elif self.devicety == 'huawei':
                 cmdlist = huawei_cmdlist
-            with ConnectHandler(device_type=self.devicety, ip=self.ip, username=self.username, password=self.passwd) as connect:
-                print("已经成功登陆交换机" + self.ip)
+            LoginInfo = {'device_type': self.devicety,
+                         'host': self.ip,
+                         'username': self.username,
+                         'password': self.passwd,
+                         # 'secret': self.enablePasswd,
+                         'port': 22}
+            if self.devicety in enableMa and pd.isnull(self.enablePasswd) is False:
+                LoginInfo.update({'secret': str(self.enablePasswd).strip()})
+            elif self.devicety not in enableMa and pd.isnull(self.enablePasswd) is False:
+                raise ValueError(f'{self.devicety} 设备厂商无enable选项。')
+            with ConnectHandler(**LoginInfo) as connect:
+            # with ConnectHandler(device_type=self.devicety, ip=self.ip, username=self.username,
+            #                   password=self.passwd) as connect:
+                if self.devicety in enableMa and pd.isnull(self.enablePasswd) is False:
+                    connect.enable()
+                logger.debug("已经成功登陆交换机" + self.ip)
                 time.sleep(2)
                 # if os.path.exists(log1 + "/" + self.ip + "-" + date_time + ".txt"):
                 #     os.remove(log1 + "/" + self.ip + "-" + date_time + ".txt")
@@ -178,26 +201,40 @@ class ssh_method(threading.Thread):
                 #     ruijie_tm(self.ip, output)
 
         except (NetmikoTimeoutException, EOFError):
-            print('\033[1;31m ***** %s 无法连接设备 \033[0m' % self.ip + '\n')
+            # print('\033[1;31m ***** %s 无法连接设备! \033[0m' % self.ip + '\n')
+            logger.error('%s 无法连接设备!' % self.ip + '\n')
             with open(f'{log1}/登陆超时设备.txt', 'a+', encoding='utf-8') as timeout:
-                timeout.write(self.ip + ' 连接设备超时\n')
+                timeout.write(self.ip + ' 连接设备超时!\n')
+                logger.error(self.ip + ' 连接设备超时!')
         except NetMikoAuthenticationException:
-            print('\033[1;31m *****%s 用户名或密码错误! \033[0m' % self.ip + '\n')
+            # print('\033[1;31m *****%s 用户名或密码错误! \033[0m' % self.ip + '\n')
+            logger.error('%s 用户名或密码错误!' % self.ip)
             with open(f'{log1}/认证失败设备.txt', 'a+', encoding='utf-8') as unauth:
                 unauth.write(self.ip + ' 用户名密码错误\n')
+                # logger.error(self.ip + ' 用户名密码错误')
         except ConfigInvalidException as conf_e:
             with open(f'{log1}/配置失败设备.txt', 'a+', encoding='utf-8') as unconfig:
                 unconfig.write(self.ip + ' 型号不匹配导致配置错误\n' + conf_e + '\n')
-            print('\033[1;31m ***** {} 型号不匹配导致配置错误\n \033[0m'.format(self.ip), conf_e)
+                # logger.debug(self.ip + ' 型号不匹配导致配置错误\n' + conf_e)
+            # print('\033[1;31m ***** {} 型号不匹配导致配置错误\n \033[0m'.format(self.ip), conf_e)
+            logger.error('{} 型号不匹配导致配置错误'.format(self.ip), conf_e)
         # except exceptions.ReadTimeout:
         #     print(f'{self.ip}读回显超时')
         except ValueError:
-            print(f'设备 {self.ip} 模板 {self.devicety} textfsm模板错误，请检查模板。')
-            with open(f'{log1}/匹配失败设备.txt', 'a+', encoding='utf-8') as muban:
-                muban.write(f'设备 {self.ip} 模板 {self.devicety} textfsm模板错误，请检查模板。\n')
+            # print(f'设备 {self.ip} 模板 {self.devicety} textfsm模板错误，请检查模板。')
+            logger.error(f'设备 {self.ip} {str(ValueError)}', exc_info=True)
+            # logger.debug(f'设备 {self.ip} 模板 {self.devicety} textfsm模板错误，请检查模板。也有可能账户被锁定', exc_info=True)
+            # logger.error(f'设备 {self.ip} 模板 {self.devicety} textfsm模板错误，请检查模板。')
+            # with open(f'{log1}/匹配失败设备.txt', 'a+', encoding='utf-8') as muban:
+            #     muban.write(f'设备 {self.ip} 模板 {self.devicety} textfsm模板错误，请检查模板。\n')
+            #     logger.error(f'设备 {self.ip} 模板 {self.devicety} textfsm模板错误，请检查模板。\n')
+            #     logger.debug(f'设备 {self.ip} 模板 {self.devicety} textfsm模板错误，请检查模板或表格信息是否错误。\n',exc_info=True)
+        except Exception:
+            logger.exception(f'设备 {self.ip} 模板 {self.devicety} 请相关开发人员。\n', exc_info=True)
         finally:
             self.queue.get()
             self.queue.task_done()
+            # logger.debug(f'{self.ip}执行完毕!')
 
 
 # 使用ip表数据登陆操作
@@ -208,8 +245,9 @@ def SSH_BY_DB(ip_list):
     for line in ip_list:
         devicety = line['device_type']
         ip = str(line['ip_address']).strip()
+        enablePW = line['enable']
         que.put(line)
-        t = ssh_method(devicety, ip, username, passwd, que)
+        t = ssh_method(devicety, ip, username, passwd, enablePW, que)
         t.start()
         th_list.append(t)
     for th in th_list:
@@ -218,8 +256,30 @@ def SSH_BY_DB(ip_list):
 
 if __name__ == '__main__':
     begin_time = time.time()
+    # 初始化报错日志文件
+    log1 = 'logs/' + time.strftime('日志_%Y年%m月%d日%H点%M分', time.localtime())
+    if not os.path.exists(log1):
+        os.makedirs(log1)
+        print(f'{log1} 文件夹创建完成')
+        logging.debug(f'{log1} 文件夹创建完成')
+
+    # logger
+    logger = logging.Logger('logger')
+    logger.setLevel(logging.DEBUG)
+    # log处理器
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setLevel(logging.ERROR)
+    fileHandler = logging.FileHandler(filename=f'{log1}/logs.log', mode='a+', encoding='utf-8')
+    # log formate
+    formatters = logging.Formatter(fmt="%(asctime)s %(thread)d %(threadName)s %(levelname)s %(message)s",
+                                   datefmt="%Y-%m-%d %H:%M-%S")
+    consoleHandler.setFormatter(formatters)
+    fileHandler.setFormatter(formatters)
+    # logger绑定处理器
+    logger.addHandler(consoleHandler)
+    logger.addHandler(fileHandler)
     # 初始化CMDB文件和报错日志文件
-    INIT_CMDB()
+    INIT_CMDB(log1)
     # 读取ip列表和命令列表
     df = pd.read_excel('ip_info.xlsx', sheet_name='ip')
     # df2 = pd.read_excel('ip_info.xlsx', sheet_name='cmd')
@@ -239,9 +299,12 @@ if __name__ == '__main__':
         host_count += 1
         # print(i)
     print(df)
+    logger.debug("操作项目:\n" + str(df))
     print('\n' * 2, '共计 \033[1;31m{}\033[0m 台'.format(host_count))
+    logger.debug('共计{}台'.format(host_count))
     print('-' * 50)
     username = input('请输入用户名:')
+    logger.debug(f'操作用户:{username}')
     passwd = getpass.getpass('请输入密码:')
     # 打开excel表
     workbook = load_workbook('CMDB.xlsx')
@@ -254,8 +317,10 @@ if __name__ == '__main__':
     print('*' * 50)
     if cost_time < 60:
         print('总共花费：{} 秒'.format(cost_time))
+        logger.debug('总共花费：{} 秒'.format(cost_time))
     else:
         mini = round(cost_time / 60)
         sec = cost_time % 60
         print('总共花费：{0} 分 {1} 秒'.format(mini, sec))
+        logger.debug('总共花费：{0} 分 {1} 秒'.format(mini, sec))
     print('*' * 50)
